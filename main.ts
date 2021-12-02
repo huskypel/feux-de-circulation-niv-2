309986f5ef29a26d50c09941bb151ce99a95bf67
huskypel <92863057+huskypel@users.noreply.github.com>
2021-12-02 11:11:36 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    if (lumiere == 1) {
-        vert()
-        basic.pause(2000)
-        orange()
-        basic.pause(2000)
-        rouge()
-        basic.pause(2000)
-    } else if (lumiere == 2) {
-        orange()
-        basic.pause(2000)
-        rouge()
-        basic.pause(2000)
-    } else if (lumiere == 3) {
-        rouge()
-        basic.pause(2000)
-    } else if (lumiere == 0) {
-        rouge()
-        basic.pause(2000)
-    }
+    lumiere = 3
 })
+function stresse () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 2)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+}
 function orange () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+function pieton () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
 }
@@ -32,47 +28,31 @@ function rouge () {
     pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.showNumber(10)
-    basic.pause(1000)
-    basic.showNumber(9)
-    basic.pause(1000)
-    basic.showNumber(8)
-    basic.pause(1000)
-    basic.showNumber(7)
-    basic.pause(1000)
-    basic.showNumber(6)
-    basic.pause(1000)
-    basic.showNumber(5)
-    basic.pause(1000)
-    basic.showNumber(4)
-    basic.pause(1000)
-    basic.showNumber(3)
-    basic.pause(1000)
-    basic.showNumber(2)
-    basic.pause(1000)
-    basic.showNumber(1)
-    basic.pause(1000)
-    basic.showNumber(0)
-    basic.pause(1000)
 }
 function vert () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
 }
 let lumiere = 0
-lumiere = 0
+lumiere = 1
 basic.forever(function () {
-    vert()
-    lumiere = 1
-    basic.pause(2000)
-    orange()
-    lumiere = 2
-    basic.pause(2000)
-    rouge()
-    lumiere = 3
-    basic.pause(2000)
-    lumiere = 0
+    if (lumiere == 1) {
+        while (lumiere == 1) {
+            vert()
+            basic.pause(5000)
+            orange()
+            basic.pause(5000)
+            rouge()
+            basic.pause(5000)
+        }
+    } else if (lumiere == 3) {
+        while (lumiere == 3) {
+            pieton()
+            basic.pause(2000)
+            lumiere = 1
+        }
+    }
 })
